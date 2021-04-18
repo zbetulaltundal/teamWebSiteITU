3564e14a9d2baf54b48fc1dd11b8a78d93b3391d
altundal67bit <zeynepbetulaltundal@gmail.com>
2021-04-18 14:43:46 +0300

    Navbar issue fixed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
-function myFunction() {
-    document.getElementById("category-btn").style.background = "none";
-  }
+$(function () {
+    $("#category-btn").blur(function(event) {
+        var screenWidth = window.innerWidth;
+        if(screenWidth < 768) {
+            $("#nav-links").collapse('hide');
+        }
+    });
+});
